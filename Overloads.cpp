93047bdd798a904c7aa10155b3f954dc6afb0b4e
David Corne <davidcorne@gmail.com>
2013-04-10 11:13:06 +0100

    Add what warning given in Overloads.cpp

--- Overloads.cpp
+++ Overloads.cpp
@@ -33,6 +33,11 @@ public:
 int main() {
   Derived d1;
   Derived d2;
+  // d1 == d2;
+  // gives
+  // request for member 'operator==' is ambiguous
+  // candidates are: bool Base_2::operator==(const Base_2&)
+  //                 bool Base_1::operator==(const Base_1&)
   cout << (d1.Base_1::operator==(d2)) << endl;
   cout << (d1.Base_2::operator==(d2)) << endl;
   return 0;
