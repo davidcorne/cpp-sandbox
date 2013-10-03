0b5737fdbe8f5d0b30d25efea8ed2f0c72cde9c2
David Corne <davidcorne@gmail.com>
2013-10-03 14:01:25 +0100

    Add a count to RecursiveMain

--- RecursiveMain.cpp
+++ RecursiveMain.cpp
@@ -1,7 +1,15 @@
 //
 // 
 
+#include <iostream>
+using namespace std;
+
+static int COUNT = 0;
+
+//=============================================================================
 int main() {
+  ++COUNT;
+  cout << COUNT << endl;
   main();
   return 0;
 }
