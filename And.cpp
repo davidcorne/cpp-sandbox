0319f9d2d79765dd3ea9ace3611818354b329425
David Corne <davidcorne@gmail.com>
2013-10-08 13:28:53 +0100

    Rename class in And.cpp and use some better IO practices.

--- And.cpp
+++ And.cpp
@@ -1,21 +1,16 @@
-//
-// 
-
 #include <iostream>
 
-using namespace std;
-
 //=============================================================================
-class T {
+class Example {
 public:
 
   //===========================================================================
   // logic operators
-  bool operator&&(const T& t) {
+  bool operator&&(const Example& t) {
     return true;
   }
   
-  bool operator||(const T& t) {
+  bool operator||(const Example& t) {
     return true;
   }
   
@@ -25,15 +20,15 @@ public:
   
   //===========================================================================
   // bitwise operators
-  bool operator&(const T& t) {
+  bool operator&(const Example& t) {
     return true;
   }
   
-  bool operator|(const T& t) {
+  bool operator|(const Example& t) {
     return true;
   }
   
-  bool operator^(const T& t) {
+  bool operator^(const Example& t) {
     return true;
   }
   
@@ -42,20 +37,20 @@ public:
   }
   
   //===========================================================================
-  // logic operators
-  T operator&=(const T& t) {
+  // logic equals operators
+  Example operator&=(const Example& t) {
     return t;
   }
   
-  T operator|=(const T& t) {
+  Example operator|=(const Example& t) {
     return t;
   }
   
-  T operator^=(const T& t) {
+  Example operator^=(const Example& t) {
     return t;
   }
   
-  T operator!=(const T& t) {
+  Example operator!=(const Example& t) {
     return t;
   }
   
@@ -66,52 +61,52 @@ int main() {
   bool a = true;
   bool b = true;
   if (a and b) {
-    cout << "\"and\" is an operator." << endl;
+    std::cout << "\"and\" is an operator.\n";
   }
   if (false or b) {
-    cout << "\"or\" is also an operator." << endl;
+    std::cout << "\"or\" is also an operator.\n";
   }
   if (not false) {
-    cout << "\"not\" is also an operator." << endl;
+    std::cout << "\"not\" is also an operator.\n";
   }
 
-  T t_1, t_2;
+  Example t_1, t_2;
   if (t_1 and t_2) {
-    cout << "\"and\" even works for classes" << endl;
+    std::cout << "\"and\" even works for classes\n";
   }
   
   if (t_1 or t_2) {
-    cout << "\"or\" even works for classes" << endl;
+    std::cout << "\"or\" even works for classes\n";
   }
   
   if (not t_1) {
-    cout << "\"and\" even works for classes" << endl;
+    std::cout << "\"and\" even works for classes\n";
   }
   
   if (t_1 bitand t_2) {
-    cout << "\"bitand\" also works for classes" << endl;
+    std::cout << "\"bitand\" also works for classes\n";
   }
   
   if (t_1 bitor t_2) {
-    cout << "\"bitor\" even works for classes" << endl;
+    std::cout << "\"bitor\" even works for classes\n";
   }
   
   if (compl t_1) {
-    cout << "\"compl\" even works for classes" << endl;
+    std::cout << "\"compl\" even works for classes\n";
   }
   
   if (t_1 xor t_2) {
-    cout << "\"xor\" even works for classes" << endl;
+    std::cout << "\"xor\" even works for classes\n";
   }
   
   t_1 and_eq t_2;
-  cout << "\"and_eq\" even works for classes" << endl;
+  std::cout << "\"and_eq\" even works for classes\n";
   
   t_1 or_eq t_2;
-  cout << "\"or_eq\" even works for classes" << endl;
+  std::cout << "\"or_eq\" even works for classes\n";
   
   t_1 xor_eq t_2;
-  cout << "\"xor_eq\" even works for classes" << endl;
-
+  std::cout << "\"xor_eq\" even works for classes\n";
+  std::cout.flush();
   return 0;
 }
