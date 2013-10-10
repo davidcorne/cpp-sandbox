27d8f0f479dd35eb3599c97b2a30b77f09d91207
David Corne <davidcorne@gmail.com>
2013-10-10 09:39:58 +0100

    Add failed attempt at borg pattern.

---
+++ Borg.cpp
@@ -0,0 +1,39 @@
+//=============================================================================
+//
+//D Inspired by
+//D http://code.activestate.com/recipes/66531-singleton-we-dont-need-no-stinkin-singleton-the-bo/
+//
+
+#include <iostream>
+
+using namespace std;
+
+//=============================================================================
+class Borg {
+public:
+
+  Borg();
+  
+private:
+  static void fill_state(Borg* b);
+
+  int m_i;
+};
+
+//=============================================================================
+Borg::Borg()
+{
+  Borg::fill_state(this);
+}
+
+//=============================================================================
+void Borg::fill_state(Borg* b)
+{
+
+}
+
+//=============================================================================
+int main() {
+  Borg b;
+  return 0;
+}
