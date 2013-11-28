6739a69640305756be4683fff9e4181993066fcf
David Corne <davidcorne@gmail.com>
2013-11-28 11:47:33 +0000

    Use a class in closure.

--- UnsupportedByCompiler/Closure.cpp
+++ UnsupportedByCompiler/Closure.cpp
@@ -10,16 +10,38 @@
 using namespace std;
 
 //=============================================================================
+template <class T>
+class ClassCounter : unary_function<void, T> {
+public:
+  ClassCounter(T start)
+    : m_count(start) {}
+  
+  T operator()()
+    {
+      ++m_count;
+      return m_count;
+    }
+  
+private:
+  T m_count;
+};
+
+//=============================================================================
 function<int(void)> counter()
 {
   static int count = 0;
   function<int(void)> increment = [&]() -> int {
-    cout << count << endl;
     ++count;
-    cout << count << endl;
     return count;
   };
   return increment;
+}
+
+//=============================================================================
+function<int(void)> class_in_counter()
+{
+  ClassCounter<int> counter(0);
+  return counter;
 }
 
 //=============================================================================
@@ -27,21 +49,47 @@ class utest_Closure : public UnitTest {
 public:
 
   void run_tests() {
-    print(DGC_CURRENT_FUNCTION);
     test_counter();
+    test_class_counter();
+    test_class_in_counter();
   }
 
 private:
 
   void test_counter();
+  void test_class_counter();
+  void test_class_in_counter();
 
 };
 
 //=============================================================================
 void utest_Closure::test_counter()
 {
-  print(__FUNCTION__);
+  print(DGC_CURRENT_FUNCTION);
   function<int(void)> func = counter();
+  test(func() == 1, "First call should be 1");
+  test(func() == 2, "Second call should be 2");
+  test(func() == 3, "Third call should be 3");
+  test(func() == 4, "Fourth call should be 4");
+}
+
+//=============================================================================
+void utest_Closure::test_class_counter()
+{
+  print(DGC_CURRENT_FUNCTION);
+  ClassCounter<int> counter(0);
+  function<int(void)> func = counter;
+  test(func() == 1, "First call should be 1");
+  test(func() == 2, "Second call should be 2");
+  test(func() == 3, "Third call should be 3");
+  test(func() == 4, "Fourth call should be 4");
+}
+
+//=============================================================================
+void utest_Closure::test_class_in_counter()
+{
+  print(DGC_CURRENT_FUNCTION);
+  function<int(void)> func = class_in_counter();
   test(func() == 1, "First call should be 1");
   test(func() == 2, "Second call should be 2");
   test(func() == 3, "Third call should be 3");
