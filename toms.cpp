2c6bc8e3d5e3d100fc6ccfd56652d71b93c4ed3e
David Corne <davidcorne@gmail.com>
2014-01-07 11:08:01 +0000

    Add file from tom

---
+++ toms.cpp
@@ -0,0 +1,18 @@
+// sent to me by skype from Tom Friday
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  const char* N = "Hello, World!";
+  int i;
+  cin >> i;
+  // <nnn> original line
+  // <nnn> for (; i++; i<10)
+  for (; i<10; i++)
+  {
+    cout << N << "\n";
+  }
+    return 0;
+}
