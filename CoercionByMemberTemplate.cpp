d1266c77f6a31564204b830c310aa11b7c334fcc
David Corne <davidcorne@gmail.com>
2014-02-17 11:52:03 +0000

    Take refs not raw pointers in CoercionByMemberTemplate

--- CoercionByMemberTemplate.cpp
+++ CoercionByMemberTemplate.cpp
@@ -1,8 +1,10 @@
 //=============================================================================
 //
-// 
+// Inspired by:
+// http://en.wikibooks.org/wiki/More_C%2B%2B_Idioms/Coercion_by_Member_Template
 
 #include <iostream>
+#include <random>
 
 #include "UnitTest.h"
 
@@ -17,53 +19,69 @@ using namespace std;
 template <class T>
 class Ptr {
 public:
-  Ptr (T* ptr)
-    : m_ptr(ptr)
+  Ptr (T& ptr)
+    : m_ptr(&ptr)
     {
+      cout << m_ptr << endl;
       assert(m_ptr);
     }
   
-  Ptr (Ptr const & p)
-    : m_ptr (p.m_ptr)
+  Ptr (const Ptr& p)
+    : m_ptr(p.m_ptr)
     {
-      std::cout << "Copy constructor\n";
     }
 
   template <class U>
-  Ptr (Ptr<U> const & p)
+  Ptr (const Ptr<U>& p)
   // Supporting coercion using member template constructor.
   // This is not a copy constructor, but behaves similarly.
-    : m_ptr (p.m_ptr) // Implicit conversion from U to T required
+    : m_ptr (convert(p)) // Implicit conversion from U to T required
     {
-      std::cout << "Coercing member template constructor\n";
     }
   
   // Copy assignment operator.
-  Ptr & operator = (Ptr const & p)
+  Ptr & operator=(const Ptr& p)
     {
       m_ptr = p.m_ptr;
-      std::cout << "Copy assignment operator\n";
       return *this;
     }
   
   template <class U>
-  Ptr & operator = (Ptr <U> const & p)
+  Ptr & operator=(const Ptr<U>& p)
   // Supporting coercion using member template assignment operator.
   // This is not the copy assignment operator, but works similarly.
     {
-      m_ptr = p.m_ptr; // Implicit conversion from U to T required
-      std::cout << "Coercing member template assignment operator\n";
+      m_ptr = convert(p); // Implicit conversion from U to T required
       return *this;
     }
   
-  T* m_ptr;
-private:
+protected:
 
   friend class utest_CoercionByMemberTemplate;
+
+  template <class U>
+  U* convert(const Ptr<U>& p)
+    {
+      // <nnn> // horrible, needs knowledge that the only member is U*
+      // <nnn> Ptr<U>* ptr_u = const_cast<Ptr<U>*>(&p);
+      // <nnn> assert(ptr_u);
+      // <nnn> U* u = reinterpret_cast<U*>(ptr_u);
+      // <nnn> assert(u);
+      // <nnn> return u;
+      struct Castable {
+        U* m_raw_pointer;
+      };
+
+      Ptr<U>* non_const_p = const_cast<Ptr<U>*>(&p);
+      assert(non_const_p);
+      Castable* castable = (Castable*)(non_const_p);
+      assert(castable);
+      U* u = (U*)(castable);
+      return u;
+      
+    }
   
-  // <nnn> template <class U>
-  // <nnn> friend class Ptr<U>;
-  
+  T* m_ptr;
 };
 
 //=============================================================================
@@ -72,6 +90,21 @@ class Base {
 
 //=============================================================================
 class Derived : public Base {
+public:
+
+  Derived()
+    : m_rand(0)
+    {
+      std::default_random_engine generator;
+      std::uniform_int_distribution<int> distribution;
+      m_rand = distribution(generator);
+    }
+  
+  bool operator==(const Derived& d) {
+    return m_rand == d.m_rand;
+  }
+
+  int m_rand;
 };
 
 //=============================================================================
@@ -80,26 +113,41 @@ public:
 
   void run_tests() {
     print(__FILE__);
-    test_();
+    test_non_null();
+    test_convert();
   }
 
 private:
 
-  void test_();
+  void test_non_null();
+  void test_convert();
 
 };
 
 //=============================================================================
-void utest_CoercionByMemberTemplate::test_()
+void utest_CoercionByMemberTemplate::test_non_null()
 {
   print(DGC_CURRENT_FUNCTION);
   Base base;
-  Ptr<Base> p_base(&base);
+  Ptr<Base> p_base(base);
   Derived derived;
-  Ptr<Derived> p_derived(&derived);
+  Ptr<Derived> p_derived(derived);
 
   Ptr<Base> new_p_base = p_derived;
   test(new_p_base.m_ptr, "Non-null pointer.");
+}
+
+//=============================================================================
+void utest_CoercionByMemberTemplate::test_convert()
+{
+  print(DGC_CURRENT_FUNCTION);
+  Derived derived;
+  Ptr<Derived> p_derived(derived);
+
+  Derived* raw_derived_ptr = p_derived.convert(p_derived);
+  test(raw_derived_ptr, "Null pointer from convert.");
+
+  test(raw_derived_ptr->m_rand == derived.m_rand, "Convert failed.");
 }
 
 //=============================================================================
