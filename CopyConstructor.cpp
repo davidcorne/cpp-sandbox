c53919a375673a014c49687d1e69661017b265d4
David Corne <davidcorne@gmail.com>
2014-03-19 15:30:10 +0000

    Add a reference to a great copy ctor/ assignment operator explanation.

---
+++ CopyConstructor.cpp
@@ -0,0 +1,9 @@
+//=============================================================================
+//
+// Nothing more than somewhere to put this link
+// http://www.icu-project.org/docs/papers/cpp_report/the_anatomy_of_the_assignment_operator.html
+
+//=============================================================================
+int main() {
+  return 0;
+}
