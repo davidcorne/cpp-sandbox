001ff06188c8ac759c0e9f21f4195957815c383e
David Corne <davidcorne@gmail.com>
2014-07-07 12:02:10 +0100

    Test number of iterations in utest_Enumeration.

--- utest_Enumeration.cpp
+++ utest_Enumeration.cpp
@@ -28,10 +28,17 @@ void utest_Enumeration::test_vector()
   print(DGC_CURRENT_FUNCTION);
   std::vector<double> array = {0, 2, 4, 6, 8};
   int i = 0;
-  for (auto pair: Enumeration<std::vector<double> >(array)) {
+  for (std::pair<int, double> pair: Enumeration<std::vector<double> >(array)) {
     test(pair.first == i, "Wrong index.");
     test(pair.second == (double)(i * 2), "Wrong value.");
     ++i;
+  }
+  test(i == 5, "Should iterate 5 times.");
+
+  Printer printer(std::cout);
+  printer.print("\nExample use.\n");
+  for (std::pair<int, double> pair: Enumeration<std::vector<double> >(array)) {
+    printer.print(pair.first, " x 2 = ", pair.second);
   }
 
 }
