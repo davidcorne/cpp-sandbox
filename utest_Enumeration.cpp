1c0bc4807b36bfd9fe38f3a10850c60c9e2ec13b
David Corne <davidcorne@gmail.com>
2014-07-07 13:29:40 +0100

    Add test for enumerate()

--- utest_Enumeration.cpp
+++ utest_Enumeration.cpp
@@ -6,6 +6,7 @@
 
 #include "UnitTest.h"
 #include "Enumeration.h"
+#include "enumerate.h"
 
 //=============================================================================
 class utest_Enumeration : public UnitTest {
@@ -14,17 +15,20 @@ public:
   void run_tests() {
     print(__FILE__);
     test_vector();
+    test_enumerate();
   }
 
 private:
 
   void test_vector();
+  void test_enumerate();
 
 };
 
 //=============================================================================
 void utest_Enumeration::test_vector()
 {
+  // This uses the Enumeration class, this makes no copies of the array.
   print(DGC_CURRENT_FUNCTION);
   std::vector<double> array = {0, 2, 4, 6, 8};
   int i = 0;
@@ -38,6 +42,29 @@ void utest_Enumeration::test_vector()
   Printer printer(std::cout);
   printer.print("\nExample use.\n");
   for (std::pair<int, double> pair: Enumeration<std::vector<double> >(array)) {
+    printer.print(pair.first, " x 2 = ", pair.second);
+  }
+
+}
+
+//=============================================================================
+void utest_Enumeration::test_enumerate()
+{
+  // Uses the free funtion enumerate. The disadvantage to this is that it
+  // creates and copies an extra container.
+  print(DGC_CURRENT_FUNCTION);
+  std::vector<double> array = {0, 2, 4, 6, 8};
+  int i = 0;
+  for (std::pair<int, double> pair: enumerate(array)) {
+    test(pair.first == i, "Wrong index.");
+    test(pair.second == (double)(i * 2), "Wrong value.");
+    ++i;
+  }
+  test(i == 5, "Should iterate 5 times.");
+
+  Printer printer(std::cout);
+  printer.print("\nExample use.\n");
+  for (std::pair<int, double> pair: enumerate(array)) {
     printer.print(pair.first, " x 2 = ", pair.second);
   }
 
