dde3b47f1d98804c21c76e164c094cf586d93eb0
David Corne <davidcorne@gmail.com>
2014-08-07 14:03:42 +0100

    Re-enable UserDefinerdLiterals.

--- UserDefinedLiterals.cpp
+++ UserDefinedLiterals.cpp
@@ -1,5 +1,4 @@
 // This only compiles with GCC 4.7 or higher
-#if 0
 // User defined literals
 // more here
 // http://akrzemi1.wordpress.com/2012/08/12/user-defined-literals-part-i/
@@ -12,55 +11,99 @@
 
 #include <iostream>
 
-using namespace std;
-
 // cooked literal
-class Length
-{
+//=============================================================================
+class Length {
 public:
-  // like an ctor enum
-  class mmLength{};
-  explicit Length(mmLength, double length)
-    : m_length(length)
-    {}
+  
+  static Length InMillimeters(double millimeters);
+  
+  static Length InInches(double inches);
 
-  void print() const
-    {
-      cout << "My length is " << m_length << "mm" << endl;
-    }
 private:
 
-  Length();
-
+  friend std::ostream& operator<<(std::ostream& os, Length length);
+  
+  explicit Length(double length_in_millimeters);
+  
   double m_length;
   // in mm
 };
 
-Length operator"" _mm(long double mil)
-{
-  return Length(Length::mmLength(), static_cast<double>(mil));
-}
+// non-member operators
+std::ostream& operator<<(std::ostream& os, Length length);
+Length operator"" _mm(long double mil);
+Length operator"" _mm(unsigned long long mil);
+Length operator"" _inches(long double mil);
+Length operator"" _inches(unsigned long long mil);
 
-Length operator"" _mm(unsigned long long mil)
-// the integer version
-{
-  return Length(Length::mmLength(), static_cast<double>(mil));
-}
-
+//=============================================================================
 int main()
 {
   Length l = 14.3_mm;
-  l.print();
+  std::cout << "My length is: " << l << std::endl;
 
   l = 15_mm;
-  l.print();
+  std::cout << "My length is: " << l << std::endl;
+
+  l = 14.3_inches;
+  std::cout << "My length is: " << l << std::endl;
+
+  l = 15_inches;
+  std::cout << "My length is: " << l << std::endl;
 
   return 0;
 }
-#else
-int main()
+
+//=============================================================================
+Length Length::InMillimeters(double millimeters)
 {
-  return 0;
+  return Length(millimeters);
 }
-#endif
+
+//=============================================================================
+Length Length::InInches(double inches)
+{
+  return Length(inches * 25.4);
+}
+
+//=============================================================================
+Length::Length(double length_in_millimeters)
+  : m_length(length_in_millimeters)
+{
+}
+
+//=============================================================================
+std::ostream& operator<<(std::ostream& os, Length length)
+{
+  os << length.m_length;
+  return os;
+}
+
+//=============================================================================
+Length operator"" _mm(long double mil)
+{
+  return Length::InMillimeters(static_cast<double>(mil));
+}
+
+//=============================================================================
+Length operator"" _mm(unsigned long long mil)
+// the integer version
+{
+  return Length::InMillimeters(static_cast<double>(mil));
+}
+
+//=============================================================================
+Length operator"" _inches(long double mil)
+{
+  return Length::InInches(static_cast<double>(mil));
+}
+
+//=============================================================================
+Length operator"" _inches(unsigned long long mil)
+// the integer version
+{
+  return Length::InInches(static_cast<double>(mil));
+}
+
 
