568addc8e2e7aa280e8337a4b177412384261a20
David Corne <davidcorne@gmail.com>
2014-08-12 11:03:46 +0100

    Add TemplateIterator.

---
+++ TemplateIterator.cpp
@@ -0,0 +1,36 @@
+//=============================================================================
+// Trying out a little type deduction, algorithms independent of iterator type
+// and trailing return type function syntax.
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+//=============================================================================
+template <typename ITER>
+auto summation(
+  ITER begin, ITER end, typename ITER::value_type start=0
+) -> typename ITER::value_type;
+
+//=============================================================================
+int main()
+{
+  std::vector<int> array = {1, 2, 3, 4};
+  std::cout << summation(std::begin(array), std::end(array)) << std::endl;
+  std::cout << summation(std::begin(array), std::end(array), 40) << std::endl;
+
+  std::vector<std::string> strings =
+    {"this ", "is ", "not ", "a " "sentence."};
+  std::cout << summation(std::begin(strings), std::end(strings), "") << std::endl;
+};
+
+//=============================================================================
+template <typename ITER>
+auto summation(ITER begin, ITER end, typename ITER::value_type start) -> typename ITER::value_type
+{
+  typename ITER::value_type result = start;
+  for (ITER it = begin; it != end; ++it) {
+    result = result + *it;
+  }
+  return result;
+}
