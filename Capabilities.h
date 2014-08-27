5809e58c848ba1f01e54c8a33389a63ecfdc5f34
David Corne <davidcorne@gmail.com>
2014-08-27 14:10:37 +0100

    Remove windows line endings from makefile and remove token from endif in Capabilities.h
    --HG--
    branch : vs

--- Capabilities.h
+++ Capabilities.h
@@ -14,5 +14,5 @@
 #if COMPILER_TYPE == COMPILER_TYPE_CLANG
 #if __has_feature(cxx_variadic_templates)
 #define VARIADIC_TEMPLATES 1
-#endif __has_feature(cxx_variadic_templates)
+#endif // __has_feature(cxx_variadic_templates)
 #endif
