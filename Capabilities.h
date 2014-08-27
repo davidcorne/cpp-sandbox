eb0a32fb6cddf973fac7d7a26d0ac632f215c93f
David Corne <davidcorne@gmail.com>
2014-08-27 14:05:59 +0100

    Add capabilities for clang.
    --HG--
    branch : vs

--- Capabilities.h
+++ Capabilities.h
@@ -3,8 +3,16 @@
 // This defines compiler capabilities.
 
 #include "Compiler.h"
+#include "Version.h"
 
 #if COMPILER_TYPE == COMPILER_TYPE_GCC
+#if VERSION > 40300
 #define VARIADIC_TEMPLATES 1
+#endif // VERSION > 40300
 #endif
 
+#if COMPILER_TYPE == COMPILER_TYPE_CLANG
+#if __has_feature(cxx_variadic_templates)
+#define VARIADIC_TEMPLATES 1
+#endif __has_feature(cxx_variadic_templates)
+#endif
