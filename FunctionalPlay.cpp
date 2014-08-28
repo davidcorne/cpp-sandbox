a4c629d4a857465f4e5fb39c76eeb09c4a3421f2
David Corne <davidcorne@gmail.com>
2014-08-28 16:44:10 +0100

    Change FunctionalPlay about a bit.

--- FunctionalPlay.cpp
+++ FunctionalPlay.cpp
@@ -13,22 +13,57 @@
 #include "Printer.h"
 
 //=============================================================================
-auto fibonnacci = [](int n) {
-  std::function<int(int)> recurse;
-  recurse = [&recurse](int i) {
-    int ret_val = 1;
-    if (i > 2) {
-      ret_val = recurse(i - 1) + recurse(i - 2);
-    }
-    return ret_val;
+auto if_func = [](bool ok, auto func_a, decltype(func_a) func_b)
+{
+  return ok ? func_a : func_b;
+};
+
+//=============================================================================
+auto id = [](auto x)
+{
+  return [x](){return x;};
+};
+
+//=============================================================================
+auto subtract = [](auto x, auto y)
+{
+  return id(x - y);
+};
+
+//=============================================================================
+auto less = [](auto x, auto y)
+{
+  return id(x < y);
+};
+  
+//=============================================================================
+auto fibonnacci = []() {
+  return [](int n) {
+    std::function<int(int)> recurse;
+    recurse = [&recurse](int i) {
+      std::function<int()> one = id(1);
+      auto again = [i, &recurse]{
+        return recurse(subtract(i, 1)()) + recurse(subtract(i, 2)());
+      };
+      return if_func(less(i, 3)(), one, again)();
+    };
+    return recurse(n);
   };
-  return recurse(n);
+};
+
+//=============================================================================
+auto range = [](auto start, auto end, std::function<void(decltype(start))> func) {
+  for (auto current = start; current < end; ++current) {
+    func(current);
+  }
 };
 
 //=============================================================================
 int main()
 {
-  print(fibonnacci(40));
+  range(1, 10, [](int i){print(i, "  ", fibonnacci()(i));});
+  range(11, 26, [](int i){print(i, " ", fibonnacci()(i));});
+  
   return 0;
 }
 
