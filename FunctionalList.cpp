60c0a9dd1e9eff3e47edb3b2cd276d57e4f04ebd
David Corne <davidcorne@gmail.com>
2014-11-11 10:27:37 +0000

    Remove last use of UnitTest.h

--- FunctionalList.cpp
+++ FunctionalList.cpp
@@ -2,10 +2,11 @@
 //
 // Based on:
 // http://bartoszmilewski.com/2013/11/13/functional-data-structures-in-c-lists/
+#include <assert.h>
 
 #include <memory>
 
-#include "UnitTest.h"
+#include <UnitCpp/Test.h>
 
 //=============================================================================
 template<typename T>
@@ -72,111 +73,109 @@ private:
 
 //=============================================================================
 template <typename T>
-class utest_List : public UnitTest {
-public:
-
-  void run_tests() {
-    print(__FILE__);
-    test_empty();
-    test_front();
-    test_pop_front();
-    test_size();
-    test_make_repeat();
-  }
-
-private:
-
-  void test_empty();
-  void test_front();
-  void test_pop_front();
-  void test_size();
-  void test_make_repeat();
-
-};
-
-//=============================================================================
-template <typename T>
-void utest_List<T>::test_empty()
+void test_empty(UnitCpp::TestCase& test_case)
 {
-  print(DGC_CURRENT_FUNCTION);
-  T list_1;
-  test(list_1.empty(), "list_1 should be empty");
-  T list_2(5, T());
-  test(!list_2.empty(), "list_2 should not be empty");
-  T list_3(0, list_2);
-  test(!list_3.empty(), "list_3 should not be empty");
+  List<T> list_1;
+  test_case.test_true(list_1.empty(), "list_1 should be empty");
+  List<T> list_2(5, List<T>());
+  test_case.test_false(list_2.empty(), "list_2 should not be empty");
+  List<T> list_3(0, list_2);
+  test_case.test_false(list_3.empty(), "list_3 should not be empty");
 }
 
 //=============================================================================
 template <typename T>
-void utest_List<T>::test_front()
+void test_front(UnitCpp::TestCase& test_case)
 {
-  print(DGC_CURRENT_FUNCTION);
-  T list_1(5, T());
-  test(list_1.front() == 5, "Front has returned the wrong thing.");
+  List<T> list_1(5, List<T>());
+  test_case.test_equal(list_1.front(), 5, "Front has returned the wrong thing.");
 }
 
 //=============================================================================
 template <typename T>
-void utest_List<T>::test_pop_front()
+void test_pop_front(UnitCpp::TestCase& test_case)
 {
-  print(DGC_CURRENT_FUNCTION);
-  T list_0;
-  T list_1(1, list_0);
-  T popped = list_1.pop_front();
-  test(popped.empty(), "Popped list should be empty.");
-  T list_2(2, list_1);
+  List<T> list_0;
+  List<T> list_1(1, list_0);
+  List<T> popped = list_1.pop_front();
+  test_case.test_true(popped.empty(), "Popped list should be empty.");
+  List<T> list_2(2, list_1);
   popped = list_2.pop_front();
-  test(popped.front() == 1, "Incorrect value popped.");
+  test_case.test_equal(popped.front(), 1, "Incorrect value popped.");
 }
 
 //=============================================================================
 template <typename T>
-void utest_List<T>::test_size()
+void test_size(UnitCpp::TestCase& test_case)
 {
-  print(DGC_CURRENT_FUNCTION);
-  T list_0;
-  test(list_0.size() == 0, "Incorrect size for empty list.");
-  T list_1(1, list_0);
-  test(list_1.size() == 1, "Incorrect size for list size 1.");
-  T list_2(2, list_1);
-  test(list_2.size() == 2, "Incorrect size for list size 2.");
-  T list_3(3, list_2);
-  test(list_3.size() == 3, "Incorrect size for list size 3.");
-  T list_4(4, list_3);
-  test(list_4.size() == 4, "Incorrect size for list size 4.");
+  List<T> list_0;
+  test_case.test_equal(list_0.size(), 0, "Incorrect size for empty list.");
+  List<T> list_1(1, list_0);
+  test_case.test_equal(list_1.size(), 1, "Incorrect size for list size 1.");
+  List<T> list_2(2, list_1);
+  test_case.test_equal(list_2.size(), 2, "Incorrect size for list size 2.");
+  List<T> list_3(3, list_2);
+  test_case.test_equal(list_3.size(), 3, "Incorrect size for list size 3.");
+  List<T> list_4(4, list_3);
+  test_case.test_equal(list_4.size(), 4, "Incorrect size for list size 4.");
 }
 
 //=============================================================================
 template <typename T>
-void utest_List<T>::test_make_repeat()
+void test_make_repeat(UnitCpp::TestCase& test_case)
 {
-  print(DGC_CURRENT_FUNCTION);
   // put each in it's own scope so that they clear up all the repeat lists
   {
     int size = 10;
-    T list = T::make_repeat(-10, size);
-    test(list.size() == size, "Wrong size of repeated list.");
+    List<T> list = List<T>::make_repeat(-10, size);
+    test_case.test_equal(list.size(), size, "Wrong size of repeated list.");
     for (int i = 0; i < size; ++i) {
-      test(list.front() == -10, "Wrong value in list.");
+      test_case.test_equal(list.front(), -10, "Wrong value in list.");
       list = list.pop_front();
     }
-    test(list.empty(), "List should be empty now.");
+    test_case.test_true(list.empty(), "List should be empty now.");
   }
   {
-    T repeated_list = T::make_repeat(5, 100);
-    test(repeated_list.size() == 100, "Wrong size of repeated list.");
+    List<T> repeated_list = List<T>::make_repeat(5, 100);
+    test_case.test_equal(repeated_list.size(), 100, "Wrong size of repeated list.");
   }
   {
-    T repeated_list = T::make_repeat(5, 1000);
-    test(repeated_list.size() == 1000, "Wrong size of repeated list.");
+    List<T> repeated_list = List<T>::make_repeat(5, 1000);
+    test_case.test_equal(repeated_list.size(), 1000, "Wrong size of repeated list.");
   }
 }
 
 //=============================================================================
-int main() {
-  utest_List<List<int> >().run_tests();
-  utest_List<List<double> >().run_tests();
-  utest_List<List<float> >().run_tests();
-  return 0;
+template <typename T>
+void test_type(UnitCpp::TestCase& test_case)
+{
+  test_empty<T>(test_case);
+  test_front<T>(test_case);
+  test_pop_front<T>(test_case);
+  test_size<T>(test_case);
+  test_make_repeat<T>(test_case);
+}
+
+//=============================================================================
+TEST(FunctionalList, int)
+{
+  test_type<int>(*this);
+}
+
+//=============================================================================
+TEST(FunctionalList, float)
+{
+  test_type<float>(*this);
+}
+
+//=============================================================================
+TEST(FunctionalList, double)
+{
+  test_type<double>(*this);
+}
+
+//=============================================================================
+int main(int argc, char** argv) 
+{
+  return UnitCpp::TestRegister::test_register().run_tests_interactive(argc, argv);
 }
