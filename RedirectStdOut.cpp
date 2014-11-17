769c9007df1dbbcaabb38361ebedb96041dfcf67
David Corne <davidcorne@gmail.com>
2014-11-17 12:00:02 +0000

    Add an example of re-directing cout.

---
+++ RedirectStdOut.cpp
@@ -0,0 +1,40 @@
+//=============================================================================
+// This shows how to redirect std::cout to a string stream.
+
+#include <cstdio>
+
+#include <iostream>
+#include <sstream>
+
+#include <UnitCpp/Test.h>
+
+//=============================================================================
+void output(std::string message)
+{
+  std::cout << message << "\n";
+}
+
+//=============================================================================
+TEST(RedirectStdOut, cout)
+{
+  std::stringstream ss;
+  // Save old buffer
+  auto cout_buffer = std::cout.rdbuf();
+  // Redirect std::cout to ss.
+  std::cout.rdbuf(ss.rdbuf());
+
+  output("Hi there.");
+  TEST_EQUAL(ss.str(), "Hi there.\n");
+  ss.str("");
+
+  // Direct std::cout back to it's original buffer.
+  std::cout.rdbuf(cout_buffer);
+
+}
+
+//=============================================================================
+int main(int argc, char** argv) 
+{
+  return UnitCpp::TestRegister::test_register().run_tests_interactive(argc, argv);
+}
+
