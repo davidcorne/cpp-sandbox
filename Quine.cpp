007f1c5ae29fa457680e31d16f3e77ef56e33a83
David Corne <davidcorne@gmail.com>
2014-11-20 12:07:12 +0000

    Add some comments to the Quine.

--- Quine.cpp
+++ Quine.cpp
@@ -1,8 +1,15 @@
+//=============================================================================
+//
+// A Quine is a self replicating program.
+
 #include<string>
 #include<iostream>
 #include <algorithm>
 
-std::string a("#include<string>\n#include<iostream>\n#include <algorithm>\n\nstd::string a(\"X\");\nint main(){\n  std::string b=a;\n  b.reserve(b.length()*3);\n  for (auto it = begin(b); it != end(b); ++it) {\n    if (*it == '\\n') {\n      *it = 'n';\n      b.insert(it, '\\\\');\n      ++it;\n    }\n    if (*it == '\"') {\n      b.insert(it, '\\\\');\n      ++it;\n    }\n    if (*it == '\\\\') {\n      b.insert(it, '\\\\');\n      ++it;\n    }\n  }\n  auto it = std::find(a.begin(), a.end(), 'X');\n  a.replace(it, it + 1, b.begin(), b.end());\n  std::cout << a;\n}\n");
+//=============================================================================
+std::string a("//=============================================================================\n//\n// A Quine is a self replicating program.\n\n#include<string>\n#include<iostream>\n#include <algorithm>\n\n//=============================================================================\nstd::string a(\"X\");\n\n//=============================================================================\nint main(){\n  std::string b=a;\n  b.reserve(b.length()*3);\n  for (auto it = begin(b); it != end(b); ++it) {\n    if (*it == '\\n') {\n      *it = 'n';\n      b.insert(it, '\\\\');\n      ++it;\n    }\n    if (*it == '\"') {\n      b.insert(it, '\\\\');\n      ++it;\n    }\n    if (*it == '\\\\') {\n      b.insert(it, '\\\\');\n      ++it;\n    }\n  }\n  auto it = std::find(a.begin(), a.end(), 'X');\n  a.replace(it, it + 1, b.begin(), b.end());\n  std::cout << a;\n}\n");
+
+//=============================================================================
 int main(){
   std::string b=a;
   b.reserve(b.length()*3);
