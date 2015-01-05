889d3f2d49ee953ec7f1829d20dad4258f886466
David Corne <davidcorne@gmail.com>
2015-01-05 09:35:10 +0000

    Add SafetyPig.

---
+++ SafetyPig.cpp
@@ -0,0 +1,29 @@
+//=============================================================================
+// From
+// https://www.quora.com/What-is-the-best-comment-in-a-source-code-that-you-have-ever-encountered/answer/Costya-Perepelitsa
+//
+// WARNING: the code that follows will make you cry;
+//          a safety pig is provided below for your benefit
+//                          _
+//  _._ _..._ .-',     _.._(`))
+// '-. `     '  /-._.-'    ',/
+//    )         \            '.
+//   / _    _    |             \
+//  |  a    a    /              |
+//  \   .-.                     ;  
+//   '-('' ).-'       ,'       ;
+//      '-;           |      .'
+//         \           \    /
+//         | 7  .__  _.-\   \
+//         | |  |  ``/  /`  /
+//        /,_|  |   /,_/   /
+//           /,_/      '`-'
+
+#include <iostream>
+
+//=============================================================================
+int main()
+{
+  std::cout << "Safety pig." << std::endl;
+  return 0;
+}
