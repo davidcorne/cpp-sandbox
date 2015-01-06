3e71f31f4f5fdae502b28aeee1cb0acce3c84cfb
David Corne <davidcorne@gmail.com>
2015-01-06 12:19:50 +0000

    Print a safety pig in SafetyPig.

--- SafetyPig.cpp
+++ SafetyPig.cpp
@@ -2,28 +2,49 @@
 // From
 // https://www.quora.com/What-is-the-best-comment-in-a-source-code-that-you-have-ever-encountered/answer/Costya-Perepelitsa
 //
-// WARNING: the code that follows will make you cry;
-//          a safety pig is provided below for your benefit
-//                          _
-//  _._ _..._ .-',     _.._(`))
-// '-. `     '  /-._.-'    ',/
-//    )         \            '.
-//   / _    _    |             \
-//  |  a    a    /              |
-//  \   .-.                     ;  
-//   '-('' ).-'       ,'       ;
-//      '-;           |      .'
-//         \           \    /
-//         | 7  .__  _.-\   \
-//         | |  |  ``/  /`  /
-//        /,_|  |   /,_/   /
-//           /,_/      '`-'
-
+/*
+ * WARNING: the code that follows will make you cry;
+ *          a safety pig is provided below for your benefit
+ * 
+ *                           _
+ *   _._ _..._ .-',     _.._(`))
+ *  '-. `     '  /-._.-'    ',/
+ *     )         \            '.
+ *    / _    _    |             \
+ *   |  a    a    /              |
+ *   \   .-.                     ;  
+ *    '-('' ).-'       ,'       ;
+ *       '-;           |      .'
+ *          \           \    /
+ *          | 7  .__  _.-\   \
+ *          | |  |  ``/  /`  /
+ *         /,_|  |   /,_/   /
+ *            /,_/      '`-'
+ *            
+ */
+// Note, the safety pig is in C style comments as there are lines ending in "\"
+// which is an error in comse compilers.
 #include <iostream>
 
 //=============================================================================
 int main()
 {
   std::cout << "Safety pig." << std::endl;
+  std::cout << ""
+               "                          _     \n"   
+               "  _._ _..._ .-',     _.._(`))   \n"
+               " '-. `     '  /-._.-'    ',/    \n"
+               "    )         \\            '.  \n"
+               "   / _    _    |             \\ \n"
+               "  |  a    a    /              | \n"
+               "  \\   .-.                      \n" 
+               "   '-('' ).-'       ,'       ;  \n"
+               "      '-;           |      .'   \n"
+               "         \\           \\    /   \n"
+               "         | 7  .__  _.-\\   \\   \n"
+               "         | |  |  ``/  /`  /     \n"
+               "        /,_|  |   /,_/   /      \n"
+               "           /,_/      '`-'       \n"
+               "                                \n";
   return 0;
 }
