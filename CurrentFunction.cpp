e85fcdb3c7d3aa53307d6f0830c7d4cdb3989253
David Corne <davidcorne@gmail.com>
2015-01-09 13:39:40 +0000

    Test CurrentFunction.h

---
+++ CurrentFunction.cpp
@@ -0,0 +1,28 @@
+//=============================================================================
+//
+// 
+
+#include <UnitCpp/Test.h>
+
+#include "CurrentFunction.h"
+
+//=============================================================================
+std::string function_name()
+{
+  return std::string(DGC_CURRENT_FUNCTION);
+}
+
+//=============================================================================
+TEST(CurrentFunction, name)
+{
+  std::string name(function_name());
+  auto index = name.find("function_name(");
+  std::cout << name << std::endl;
+  TEST_NOT_EQUAL(index, std::string::npos);
+}
+
+//=============================================================================
+int main(int argc, char** argv) 
+{
+  return UnitCpp::TestRegister::test_register().run_tests_interactive(argc, argv);
+}
