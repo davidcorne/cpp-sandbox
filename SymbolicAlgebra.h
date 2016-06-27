84e239893ff93708d2f5d3d9a8ba901a9e0c023d
David Corne <davidcorne@gmail.com>
2016-06-27 14:52:33 +0100

    Add actual solving to SymbolicAlgebra

--- SymbolicAlgebra.h
+++ SymbolicAlgebra.h
@@ -6,9 +6,13 @@
 #ifndef SymbolicAlgebra_H
 #define SymbolicAlgebra_H
 
-#include <cmath>
+#include <algorithm>
+#include <complex>
 #include <iostream>
 #include <vector>
+
+#include <cmath>
+#include <cassert>
 
 //=============================================================================
 class Term {
@@ -27,20 +31,69 @@ private:
 };
 
 //=============================================================================
-class Expression {
+class PolynomialExpression {
 public:
 
-  Expression(std::initializer_list<Term> initializer);
+  PolynomialExpression(std::initializer_list<Term> initializer);
 
   std::ostream& out(std::ostream& os, char variable) const;
+
+  int order() const;
+
+  double coefficient(int order) const;
   
 private:
 
+  bool has(int order) const;
+  // Has a term of "order".
+  
+  const Term& at(int order) const;
+  // Get the term of "order".
+
+  std::vector<Term>::const_iterator find(int order) const;
+  // Find a term of given order.
+  
+  struct TermOrdinality {
+    bool operator()(const Term& a, const Term& b) const {
+      return a.exponent() > b.exponent();
+    }
+  };
   std::vector<Term> m_terms;
 };
 
 //=============================================================================
-std::ostream& operator<<(std::ostream& os, const Expression& expression)
+class Root {
+public:
+
+  Root(std::complex<double> solution);
+  
+  std::complex<double> solution() const;
+  
+private:
+
+  std::complex<double> m_solution;
+};
+
+//=============================================================================
+class PolynomialExpressionSolver {
+public:
+
+  std::vector<Root> solve(const PolynomialExpression& expression) const;
+  
+private:
+
+  std::vector<Root> solve_quadratic(
+    const PolynomialExpression& expression
+  ) const;
+  
+  std::vector<Root> solve_linear(
+    const PolynomialExpression& expression
+  ) const;
+  
+};
+
+//=============================================================================
+std::ostream& operator<<(std::ostream& os, const PolynomialExpression& expression)
 {
   return expression.out(os, 'x');
 }
@@ -65,13 +118,56 @@ int Term::exponent() const
 }
 
 //=============================================================================
-Expression::Expression(std::initializer_list<Term> initializer)
+PolynomialExpression::PolynomialExpression(std::initializer_list<Term> initializer)
   : m_terms(initializer)
 {
+  std::sort(begin(m_terms), end(m_terms), TermOrdinality());
 }
 
 //=============================================================================
-std::ostream& Expression::out(std::ostream& os, char variable) const
+int PolynomialExpression::order() const
+{
+  int l_order = 0;
+  if (!m_terms.empty()) {
+    l_order = m_terms.front().exponent();
+  }
+  return l_order;
+}
+
+//=============================================================================
+double PolynomialExpression::coefficient(int order) const
+{
+  auto it = find(order);
+  return it != end(m_terms) ? it->coefficient() : 0;
+}
+  
+//=============================================================================
+bool PolynomialExpression::has(int order) const
+// Has a term of "order".
+{
+  return find(order) != end(m_terms);
+}
+  
+//=============================================================================
+const Term& PolynomialExpression::at(int order) const
+// Get the term of "order".
+{
+  assert(has(order) && "Can't get a term with given order.");
+  return *find(order);
+}
+
+//=============================================================================
+std::vector<Term>::const_iterator PolynomialExpression::find(int order) const
+{
+  return std::find_if(
+    begin(m_terms),
+    end(m_terms),
+    [order](const Term& term)->bool{return term.exponent() == order;}
+  );
+}
+
+//=============================================================================
+std::ostream& PolynomialExpression::out(std::ostream& os, char variable) const
 {
   for (auto it = begin(m_terms); it != end(m_terms); ++it) {
     const Term& term = *it;
@@ -100,6 +196,80 @@ std::ostream& Expression::out(std::ostream& os, char variable) const
     }
   }
   return os;
+}
+
+
+//=============================================================================
+Root::Root(std::complex<double> solution)
+  : m_solution(solution)
+{
+}
+
+//=============================================================================
+std::complex<double> Root::solution() const
+{
+  return m_solution;
+}
+
+//=============================================================================
+std::vector<Root> PolynomialExpressionSolver::solve(
+  const PolynomialExpression& expression
+) const
+{
+  int order = expression.order();
+  std::vector<Root> roots;
+  if (order == 1) {
+    roots = solve_linear(expression);
+  } else if (order == 2) {
+    roots = solve_quadratic(expression);
+  }
+  return roots;
+}
+
+
+//=============================================================================
+std::vector<Root> PolynomialExpressionSolver::solve_quadratic(
+  const PolynomialExpression& expression
+) const
+{
+  assert(expression.order() == 2 && "Should be order 2");
+  double a = expression.coefficient(2);
+  double b = expression.coefficient(1);
+  double c = expression.coefficient(0);
+  // (-b +- sqrt(b^2 - 4ac)) / 2a
+  double determinant = (b*b) - (4*a*c);
+  std::vector<Root> roots;
+  if (determinant < 0) {
+    // deal with complex roots
+  } else if (determinant == 0) {
+    roots.push_back(Root(std::complex<double>((-1 * b) / (2 * a))));
+  } else {
+    roots.push_back(
+      Root(std::complex<double>(((-1 * b) + std::sqrt(determinant)) / (2 * a)))
+    );
+    roots.push_back(
+      Root(std::complex<double>(((-1 * b) - std::sqrt(determinant)) / (2 * a)))
+    );
+  }
+  
+  return roots;
+}
+
+//=============================================================================
+std::vector<Root> PolynomialExpressionSolver::solve_linear(
+  const PolynomialExpression& expression
+) const
+{
+  assert(expression.order() == 1 && "Should be order 1");
+  std::vector<Root> roots;
+  // ax + b = 0
+  // x = -b/a
+  double a = expression.coefficient(1);
+  double b = expression.coefficient(0);
+  if (a != 0) {
+    roots.push_back(Root((-1 * b) / a));
+  }
+  return roots;
 }
 
 #endif
--- utest_SymbolicAlgebra.cpp
+++ utest_SymbolicAlgebra.cpp
@@ -5,13 +5,14 @@
 #include "SymbolicAlgebra.h"
 #include "SymbolicAlgebra.h"
 
-#include <UnitCpp.h>
+#include <set>
 
+#include <UnitCpp.h>
 
 //=============================================================================
 TEST(SymbolicAlgebra, print)
 {
-  Expression expression{Term(1, 2), Term(2, 1), Term(1, 0)};
+  PolynomialExpression expression{Term(1, 2), Term(2, 1), Term(1, 0)};
   std::stringstream ss;
   ss << expression;
   TEST_EQUAL(ss.str(), "x^2 + 2x + 1");
@@ -21,9 +22,91 @@ TEST(SymbolicAlgebra, print)
   TEST_EQUAL(ss.str(), "y^2 + 2y + 1");
 
   ss.str("");
-  expression = Expression{Term(-1, 2), Term(2, 1), Term(-1, 0)};
+  expression = PolynomialExpression{Term(-1, 2), Term(2, 1), Term(-1, 0)};
   expression.out(ss, 'z');
   TEST_EQUAL(ss.str(), "-z^2 + 2z - 1");
+  
+}
+
+//=============================================================================
+TEST(SymbolicAlgebra, getter)
+{
+  PolynomialExpression expression(
+    {Term(1, 1), Term(2, 2), Term(3, 3), Term(4, 4), Term(5, 5)}
+  );
+  TEST_EQUAL(expression.coefficient(0), 0);
+  TEST_EQUAL(expression.coefficient(1), 1);
+  TEST_EQUAL(expression.coefficient(2), 2);
+  TEST_EQUAL(expression.coefficient(3), 3);
+  TEST_EQUAL(expression.coefficient(4), 4);
+  TEST_EQUAL(expression.coefficient(5), 5);
+  TEST_EQUAL(expression.coefficient(6), 0);
+  TEST_EQUAL(expression.coefficient(7), 0);
+  TEST_EQUAL(expression.coefficient(8), 0);
+  TEST_EQUAL(expression.coefficient(9), 0);
+  TEST_EQUAL(expression.coefficient(10), 0);
+}
+
+//=============================================================================
+TEST(SymbolicAlgebra, order)
+{
+  {
+    PolynomialExpression expression{};
+    TEST_EQUAL(expression.order(), 0);
+  }
+  {
+    PolynomialExpression expression{Term(5, 1)};
+    TEST_EQUAL(expression.order(), 1);
+  }
+  {
+    PolynomialExpression expression{Term(4, 1), Term(3, 2)};
+    TEST_EQUAL(expression.order(), 2);
+  }
+  {
+    PolynomialExpression expression{Term(4, 3)};
+    TEST_EQUAL(expression.order(), 3);
+  }
+}
+//=============================================================================
+TEST(SymbolicAlgebra, solve)
+{
+  {
+    // x - 4
+    // roots: 4
+    PolynomialExpression expression({Term(1, 1), Term(-4, 0)});
+    std::vector<Root> roots = PolynomialExpressionSolver().solve(expression);
+    TEST_EQUAL(roots.size(), 1u);
+    TEST_EQUAL(roots.front().solution(), std::complex<double>(4));
+  }
+  {
+    // 2x + 9
+    // roots: -4.5
+    PolynomialExpression expression({Term(2, 1), Term(9, 0)});
+    std::vector<Root> roots = PolynomialExpressionSolver().solve(expression);
+    TEST_EQUAL(roots.size(), 1u);
+    TEST_EQUAL(roots.front().solution(), std::complex<double>(-4.5));
+  }
+  {
+    // x^2 - 1
+    // roots: 1, -1
+    PolynomialExpression expression({Term(1, 2), Term(-1, 0)});
+    std::vector<Root> roots = PolynomialExpressionSolver().solve(expression);
+    TEST_EQUAL(roots.size(), 2u);
+    std::vector<std::complex<double>> root_set;
+    for (const Root& root : roots) {
+      root_set.push_back(root.solution());
+    }
+    TEST_NOT_EQUAL(
+      std::find(begin(root_set), end(root_set), std::complex<double>(1)),
+      end(root_set),
+      "Should find 1 as a root."
+    );
+    TEST_NOT_EQUAL(
+      std::find(begin(root_set), end(root_set), std::complex<double>(-1)),
+      end(root_set),
+      "Should find -1 as a root."
+    );
+  }
   
 }
 
