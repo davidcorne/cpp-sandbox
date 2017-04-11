50a63c6e199562ee770ca9acb3106c76cd54f0c3
David Corne <davidcorne@gmail.com>
2017-04-11 16:56:13 +0100

    Use std::alignment_of.

--- AlignedStorage.h
+++ AlignedStorage.h
@@ -1,12 +1,10 @@
 #ifndef AlignedStorage_H
 #define AlignedStorage_H
 
-#include "AlignOf.h"
-
 #include <type_traits>
 
 template <typename tTYPE>
 using AlignedStorage =
-  typename std::aligned_storage<sizeof(tTYPE), DGC_ALIGNOF(tTYPE)>::type;
+  typename std::aligned_storage<sizeof(tTYPE), std::alignment_of<tTYPE>::value>::type;
 
 #endif
